68e9602f2cc43546d145f2a99254dfce8bf548da
Marcin Kwiatkowski <marcin@Marcins-Mac.local>
2013-06-11 16:04:09 -0700

    Initial Commit

---
+++ TestApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Marcin Kwiatkowski on 11/06/2013.
+//  Copyright (c) 2013 Marcin Kwiatkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestAppTests/TestAppTests.m
@@ -0,0 +1,36 @@
+//
+//  TestAppTests.m
+//  TestAppTests
+//
+//  Created by Marcin Kwiatkowski on 11/06/2013.
+//  Copyright (c) 2013 Marcin Kwiatkowski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestAppTests : XCTestCase
+
+@end
+
+@implementation TestAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
